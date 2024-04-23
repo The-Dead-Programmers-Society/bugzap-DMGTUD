148d6263b61708f868bcc69082fc705687d992f9
DMGTUD <145370309+DMGTUD@users.noreply.github.com>
2024-04-24 00:58:03 +0100

    GuitarComplete

--- Daniel Commits/Guitar/guitarCode.gd
+++ Daniel Commits/Guitar/guitarCode.gd
@@ -6,3 +6,138 @@ extends Control
 func _on_eddie_pressed():
 	#$AudioStreamPlayer2D.set_pitch_scale(0.3)
 	$EddieAudio.play()
+
+
+func _on_ate_pressed():
+	$AteAudio.play()
+
+
+
+func _on_dynamite_pressed():
+	$DynamiteAudio.play()
+
+
+
+
+func _on_good_pressed():
+	$GoodAudio.play()
+
+
+
+
+func _on_bye_pressed():
+	$ByeAudio.play()
+
+
+
+
+func _on_eddie_2_pressed():
+	$Eddie2Audio.play()
+
+func _on_eo_pressed():
+	$EddieAudio.set_pitch_scale(1)
+
+func _on_ao_pressed():
+	$AteAudio.set_pitch_scale(1)
+
+func _on_do_pressed():
+	$DynamiteAudio.set_pitch_scale(1)
+
+func _on_bo_pressed():
+	$GoodAudio.set_pitch_scale(1)
+
+func _on_go_pressed():
+	$GoodAudio.set_pitch_scale(1)
+
+func _on_eeo_pressed():
+	$Eddie2Audio.set_pitch_scale(1)
+
+func _on_e1_pressed():
+	$EddieAudio.set_pitch_scale(1.1)
+
+func _on_a1_pressed():
+	$AteAudio.set_pitch_scale(1.1)
+
+func _on_d1_pressed():
+	$DynamiteAudio.set_pitch_scale(1.1)
+
+func _on_b1_pressed():
+	$GoodAudio.set_pitch_scale(1.1)
+
+func _on_g1_pressed():
+	$GoodAudio.set_pitch_scale(1.1)
+
+func _on_ee1_pressed():
+	$Eddie2Audio.set_pitch_scale(1.1)
+
+func _on_e2_pressed():
+	$EddieAudio.set_pitch_scale(1.2)
+
+func _on_a2_pressed():
+	$AteAudio.set_pitch_scale(1.2)
+
+func _on_d2_pressed():
+	$DynamiteAudio.set_pitch_scale(1.2)
+
+func _on_b2_pressed():
+	$GoodAudio.set_pitch_scale(1.2)
+
+func _on_g2_pressed():
+	$GoodAudio.set_pitch_scale(1.2)
+
+func _on_ee2_pressed():
+	$Eddie2Audio.set_pitch_scale(1.2)
+
+func _on_e3_pressed():
+	$EddieAudio.set_pitch_scale(1.3)
+
+func _on_a3_pressed():
+	$AteAudio.set_pitch_scale(1.3)
+
+func _on_d3_pressed():
+	$DynamiteAudio.set_pitch_scale(1.3)
+
+func _on_b3_pressed():
+	$GoodAudio.set_pitch_scale(1.3)
+
+func _on_g3_pressed():
+	$GoodAudio.set_pitch_scale(1.3)
+
+func _on_ee3_pressed():
+	$Eddie2Audio.set_pitch_scale(1.3)
+	
+func _on_e4_pressed():
+	$EddieAudio.set_pitch_scale(1.4)
+
+func _on_a4_pressed():
+	$AteAudio.set_pitch_scale(1.4)
+
+func _on_d4_pressed():
+	$DynamiteAudio.set_pitch_scale(1.4)
+
+func _on_b4_pressed():
+	$GoodAudio.set_pitch_scale(1.4)
+
+func _on_g4_pressed():
+	$GoodAudio.set_pitch_scale(1.4)
+
+func _on_ee4_pressed():
+	$Eddie2Audio.set_pitch_scale(1.4)
+
+func _on_e5_pressed():
+	$EddieAudio.set_pitch_scale(1.5)
+
+func _on_a5_pressed():
+	$AteAudio.set_pitch_scale(1.5)
+
+func _on_d5_pressed():
+	$DynamiteAudio.set_pitch_scale(1.5)
+
+func _on_b5_pressed():
+	$GoodAudio.set_pitch_scale(1.5)
+
+func _on_g5_pressed():
+	$GoodAudio.set_pitch_scale(1.5)
+
+func _on_ee5_pressed():
+	$Eddie2Audio.set_pitch_scale(1.5)
